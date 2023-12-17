499a5731f71404b07d05e6889071501480a00f51
JerrryCai <95020130+JerrryCai@users.noreply.github.com>
2023-12-17 17:44:10 +1000

    minor adjustment

--- post/index.js
+++ post/index.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const fs = require('fs');
 const cors = require('cors');
 
 const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/posts', (req, res) => {});
+app.get('/api/v1/blogs', (req, res) => {
+  fs.readFile('data.json', 'utf8', (err, data) => {
+    if (err) {
+      res.status(500).send({ error: 'Error reading file' });
+      return;
+    }
+    res.send(JSON.parse(data).blogs);
+  });
+});
 
-app.post('/posts', (req, res) => {});
+app.post('/api/v1/blogs', (req, res) => {});
 
 app.listen(4000, () => {
   console.log('Listening on 4000');
