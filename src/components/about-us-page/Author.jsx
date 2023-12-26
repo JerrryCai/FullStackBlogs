7a19bb5c3559018830a07fb2dfefe4faae1c988c
YangGe25 <S3911292@student.rmit.edu.au>
2023-12-26 18:38:37 +1100

    change Author and BlogHeader css

--- src/components/about-us-page/Author.jsx
+++ src/components/about-us-page/Author.jsx
@@ -2,29 +2,30 @@ import React from 'react';
 import { FaLinkedin } from 'react-icons/fa6';
 import { MdEmail } from 'react-icons/md';
 import imagePlaceholder from '../../assets/images/Image-Placeholder.png';
+import styles from "./Author.module.css"
 /**
  * The author component in about us page
  * @return {React.Component}
  */
 export default function Author() {
   return (
-    <div className="w-[25rem] min-w-[25rem]">
+    <div className={styles.container}>
       <img
         src={imagePlaceholder}
         alt="avatar"
-        className="h-20 w-20 rounded-full object-cover"
+        className={styles.image}
       />
-      <div className="my-6">
-        <div className="mb-4">
-          <p className="text-xl font-semibold text-text-100">FULL NAME</p>
-          <p className="text-lg font-normal">JOB TITLE</p>
+      <div className={styles.informationContainer}>
+        <div className={styles.information}>
+          <p className={styles.fullName}>FULL NAME</p>
+          <p className={styles.jobTitle}>JOB TITLE</p>
         </div>
-        <p className="text-base">
+        <p className={styles.description}>
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Suspendisse
           varius enim in eros elementum tristique.
         </p>
       </div>
-      <div className="flex items-center gap-[0.875rem]">
+      <div className={styles.iconContainer}>
         <button>
           <FaLinkedin size={24} />
         </button>
--- src/components/blog-page/BlogHeader.jsx
+++ src/components/blog-page/BlogHeader.jsx
@@ -5,30 +5,32 @@ import { LuDot } from 'react-icons/lu';
 import { IoIosLink, IoLogoLinkedin } from 'react-icons/io';
 import { IoLogoFacebook } from 'react-icons/io5';
 import { BsTwitterX } from 'react-icons/bs';
+import styles from "./BlogHeader.module.css"
+import { symbol } from 'prop-types';
 /**
  * Blog header of every posts
  * @return {React.Component}
  */
 export default function BlogHeader() {
   return (
-    <div className="md:mx-16 md:my-28">
-      <div className="gap-20 md:flex">
-        <div className="flex w-1/2 flex-col">
-          <div className="flex items-center gap-2 text-accent-100 md:mb-10">
+    <div className={styles.container}>
+      <div className={styles.subContainer}>
+        <div className={styles.blogContainer}>
+          <div className={styles.categoryContainer}>
             <p>Blog</p>
             <div>
               <FaAngleRight />
             </div>
-            <p className="text-primary-100">CATEGORY</p>
+            <p className={styles.category}>CATEGORY</p>
           </div>
           <div>
-            <h1 className="text-5xl font-bold md:mb-12">
+            <h1 className={styles.blogTitle}>
               Blog title Header will go here
             </h1>
             <p>
-              By <span className="text-sm font-semibold">FULL NAME</span>
+              By <span className={styles.fullName}>FULL NAME</span>
             </p>
-            <div className="flex items-center gap-2">
+            <div className={styles.blogInformationContainer}>
               <p>11 Jan 2022</p>
               <p>
                 <LuDot size={20} />
@@ -36,29 +38,29 @@ export default function BlogHeader() {
               <p>5 min read</p>
             </div>
           </div>
-          <div className="mt-auto mb-8">
-            <p className="text-lg font-semibold mb-4">Share this post</p>
-            <div className="flex items-center gap-2">
-              <button className="rounded-full bg-text-main p-1">
+          <div className={styles.shareContainer}>
+            <p className={styles.shareText}>Share this post</p>
+            <div className={styles.iconContainer}>
+              <button className={styles.icon}>
                 <IoIosLink />
               </button>
-              <button className="rounded-full bg-text-main p-1">
+              <button className={styles.icon}>
                 <IoLogoLinkedin />
               </button>
-              <button className="rounded-full bg-text-main p-1">
+              <button className={styles.icon}>
                 <BsTwitterX />
               </button>
-              <button className="rounded-full bg-text-main p-1">
+              <button className={styles.icon}>
                 <IoLogoFacebook />
               </button>
             </div>
           </div>
         </div>
-        <div className="w-1/2">
+        <div className={styles.imageContainer}>
           <img
             src={imagePlaceholder}
             alt="post image"
-            className="h-[395px] w-full object-cover"
+            className={styles.image}
           />
         </div>
       </div>
