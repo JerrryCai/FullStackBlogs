586673d1e35c52ad386b2b7da6537906df0bd15f
YangGe25 <S3911292@student.rmit.edu.au>
2023-12-26 17:50:23 +1100

    finish pages css change

--- src/pages/Blogs.jsx
+++ src/pages/Blogs.jsx
@@ -2,13 +2,14 @@ import React from 'react';
 import BlogHeader from '../components/blogs-page/BlogHeader';
 import HighlightBlogPosts from '../components/blogs-page/HighlightBlogPosts';
 import MainBlogPosts from '../components/blogs-page/MainBlogPosts';
+import styles from "./Blogs.module.css"
 /**
  * Blogs component
  * @return {React.Component}
  */
 export default function Blogs() {
   return (
-    <div className="px-8 py-14 md:px-16 md:py-28">
+    <div className={styles.container}>
       <BlogHeader />
       <HighlightBlogPosts />
       <MainBlogPosts />
